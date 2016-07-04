4eb98c65c40a600dc9b566a1e896e755f547f8a4
Nick McConnell <ngmcconnell@yahoo.com>
2016-07-04 18:14:30 -0500

    fixed listening

--- server.js
+++ server.js
@@ -4,3 +4,6 @@ app.set('port', (process.env.PORT || 5000));
 app.get('/', function(req, res){ 
   res.send('Message from Nick'); 
 });
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
