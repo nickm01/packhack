f524e896c693df9c8a7a70afb3c6cf1c59b4c835
Nick McConnell <ngmcconnell@yahoo.com>
2016-07-04 18:58:08 -0500

    try env variables out

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ app.set('port', (process.env.PORT || 5000));
 app.get('/', function(req, res){ 
   res.send('Message from Nick'); 
 });
+app.get('/times', function(request, response) {
+    var result = ''
+    var times = process.env.TIMES || 5
+    for (i=0; i < times; i++)
+      result += i + ' ';
+  response.send(result);
+});
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
