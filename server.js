764bf406289915ad9d012182b6d08683bfb46841
Nick McConnell <ngmcconnell@yahoo.com>
2016-08-07 10:58:16 -0500

    twilio fix

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ router.route("/types/:id")
 router.route("/twilio")
     .get(function(req,res){
         var response = {};
-        console.log('From Twilio ' + req.query.from + " message:" + req.query.stuff);
+        console.log('From Twilio ' + req.param('from') + " message:" + req.param('stuff'));
         response = {"success": true};
         res.json(response);
       });
