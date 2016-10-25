d58810d35f9fad1d860ec92d8bb22b0fcdcdda52
Nick McConnell <ngmcconnell@yahoo.com>
2016-10-24 22:56:41 -0500

    welcome

--- server.js
+++ server.js
@@ -82,8 +82,7 @@ router.route("/twilio")
         console.log('*** Send welcome');
         response = true;
         var welcomeSendPhoneNumber = bodyText.substr(10);
-        admin.sendSms('+13476979750','Welcome to FLOCK!\nYou now have the power to crowdsource your family lists. Learn more - text the word “flock” to this number to see a list of available commands.\nHave fun!\n❤️FLOCK
-');
+        admin.sendSms('+13476979750','Welcome to FLOCK!\nYou now have the power to crowdsource your family lists. Learn more - text the word “flock” to this number to see a list of available commands.\nHave fun!\n❤️FLOCK');
 
       // Get list items
       } else if (bodyText.startsWith("get #")) {
