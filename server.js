9be281aee2cb10b09b57b82ee2e961c0724e7650
Nick McConnell <ngmcconnell@yahoo.com>
2016-10-24 23:06:20 -0500

    welcome

--- server.js
+++ server.js
@@ -78,11 +78,13 @@ router.route("/twilio")
 
 
       // Basic welcome message
-      } else if (bodyText.startsWith("** welcome")) {
+      } else if (bodyText.startsWith("** welcome ")) {
         console.log('*** Send welcome');
         response = true;
-        var welcomeSendPhoneNumber = bodyText.substr(10);
-        admin.sendSms('+13476979750','Welcome to FLOCK!\nYou now have the power to crowdsource your family lists. Learn more - text the word “flock” to this number to see a list of available commands.\nHave fun!\n❤️FLOCK');
+        var welcomeSendUserId = bodyText.substr(12);
+        mongoOp.FamilyMembers.findOne({'id': welcomeSendUserId}, 'phoneNumber', function(err, familyMember) {
+          admin.sendSms(familyMember.phoneNumber,'Welcome to FLOCK!\nYou now have the power to crowdsource your family lists. Learn more - text the word “flock” to this number to see a list of available commands.\nHave fun!\n❤️FLOCK');
+        });
 
       // Get list items
       } else if (bodyText.startsWith("get #")) {
