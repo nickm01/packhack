9aa939515fcf3e638acdff90fedbfa83354b7684
Nick McConnell <ngmcconnell@yahoo.com>
2016-11-12 18:55:53 -0600

    scheduler

--- sendsms.js
+++ sendsms.js
@@ -2,12 +2,7 @@ var config = require('./config');
 var client = require('twilio')(config.accountSid, config.authToken);
 
 module.exports.sendSms = function(to, message, completion) {
-  client.messages.create({
-    body: message,
-    to: to,
-    from: config.sendingNumber
-    // mediaUrl: 'http://www.yourserver.com/someimage.png'
-  }, function(err, data) {
+  client.messages.create({ body: message, to: to, from: config.sendingNumber}, function(err, data) {
     if (err) {
       console.error('Could not send message');
       console.error(err);
