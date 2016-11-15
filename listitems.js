ad08604b3619343b8af8e66a4b9013401e14e6f1
Nick McConnell <ngmcconnell@yahoo.com>
2016-11-14 21:28:25 -0600

    scheduler

---
+++ listitems.js
@@ -0,0 +1,20 @@
+var config = require('./config');
+var mongoOp = require('./model/mongo');
+var logging = require('./logging');
+var stringProcessor = require('./stringprocessor');
+
+function deleteListItemByName(familyId, listKey, listItemName, callback) {
+	mongoOp.ListItems.remove({"listKey" : listKey, "listItemName" : listItemName, 'familyId': familyId}, function(err, removeResult) {
+	  if (err) {
+	    logging.logError(fromPhoneNumber, familyId, bodyText, err);
+	    callback('Error removing ' + listItemName + ' 😦');
+	    return;
+	  }
+	  console.log('----removed ' + listItemName + ' ' + removeResult.result.n);
+	  if (removeResult.result.n === 0) {
+	    callback(listItemName + ' doesn't exist in #'' + listName + '.'); 
+	  } else {
+	  	callback(null);
+	  }
+	});
+}
