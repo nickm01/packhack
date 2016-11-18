6726235a1e341adedd4e7a0d91d41a8626c4fe11
Nick McConnell <ngmcconnell@yahoo.com>
2016-11-17 23:09:46 -0600

    scheduler

--- dates.js
+++ dates.js
@@ -18,7 +18,7 @@ function processDateAndTitleFromText (inputText, callback) {
   sherlock._setNow(nowLocalDate)
   var sherlocked = sherlock.parse(inputText)
   var startDateString = '' + sherlocked.startDate
-  var startDateTrimmed = startDateString.substring(0, startDateString.length - 15) + ' GMT-0006 (UTC)'
+  var startDateTrimmed = startDateString.substring(0, startDateString.length - 15) + ' GMT-06:00 (UTC)'
   var startDateLocal = moment(startDateTrimmed)
   var startDateGMT = new Date(startDateLocal)
   console.log('startDateTrimmed:' + startDateTrimmed + ' startDateLocal:' + startDateLocal.format() + ' startDateGMT:' + startDateGMT)
