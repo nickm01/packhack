88c24e5012a5c3370c31b09aff01ef394df4d3a6
Nick McConnell <ngmcconnell@yahoo.com>
2016-12-27 17:26:37 -0500

    scheduler

--- stringprocessor.js
+++ stringprocessor.js
@@ -1,15 +1,20 @@
-function getFirstWord(str) {
-  if (str.indexOf(' ') === -1)
-    return str;
-  else
-    return str.substr(0, str.indexOf(' '));
-};
+function getFirstWord (str) {
+  if (str.indexOf(' ') === -1) {
+    return str
+  } else {
+    return str.substr(0, str.indexOf(' '))
+  }
+}
 
-function removeFirstWord(str) {
-  return str.substr(str.indexOf(" ") + 1);
+function removeFirstWord (str) {
+  if (str.indexOf(' ') === -1) {
+    return str
+  } else {
+    return str.substr(str.indexOf(' ') + 1)
+  }
 }
 
 module.exports = {
-   getFirstWord,
-   removeFirstWord
+  getFirstWord,
+  removeFirstWord
 }
