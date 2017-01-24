5998e7734f6c182a033b3b5fcb9a3be03a46074d
Nick McConnell <ngmcconnell@yahoo.com>
2017-01-23 21:56:16 -0600

    promises

--- bin/scheduler2.js
+++ bin/scheduler2.js
@@ -14,95 +14,135 @@ function processScheduler () {
     if (err) {
       logging.logError('scheduler', '', '', err)
     } else {
+      var listItemPromises = []
       listItems.forEach(function (listItem) {
-        var familyId = listItem.familyId
         var reminderDate = new Date(listItem.reminderWhen)
         var now = new Date()
         console.log('now: ' + now)
         console.log('Scheduler2:' + reminderDate + ' userId:' + listItem.reminderUserId + ' listItemName:' + listItem.listItemName)
 
-        if (reminderDate < now) {
-          // Remove listItem first
-          listItemFunctions.deleteListItemByName(familyId, listItem.listKey, listItem.listItemName, function (err) {
-            if (err) {
-              return
-            } else {
-              // Send to single or multiple family members
-              multipleOrSingleFamilyMembersForReminderUserId(familyId, listItem.reminderUserId, function (err, familyMember) {
-                if (err) {
-                  logging.logError('0', familyId, listItem, err)
-                  return
-                }
-                count++
+        // TODO: Do the delete at the end of a promise chain
+        if (reminderDate < now || reminderDate > now) { // TODO: Remove second clause
+          console.log('A1: ' + listItem)
+          listItemPromises.push(
+            deleteListItemByNamePromise(listItem)
+            .then(multipleOrSingleFamilyMembersForReminderUserIdPromise)
+            .then(function (something) {
+              console.log('X: ' + something + ' count ' + something.length)
+              return something
+            })
 
-                // textForReminder(listItem, function (err, text) {
-                //   if (err) {
-                //   }
-                //   text = '⏰ ' + text
-                //   console.log('Count++ ' + count + ' pn:' + familyMember.phoneNumber + ' ' + familyMember.name + ':' + text)
-                //   logging.log(familyMember.phoneNumber, familyId, text, 'Reminder', '')
-                //   sendSms.sendSms(familyMember.phoneNumber, text, function () {
-                //     count--
-                //     console.log('Count-- ' + count)
-                //     if (count === 0) {
-                //       process.exit()
-                //     }
-                //   })
-                // })
-              })
-            }
-          })
+          )
+          // Remove listItem first
+          // listItemFunctions.deleteListItemByName(familyId, listItem.listKey, listItem.listItemName, function (err) {
+          //   if (err) {
+          //     return
+          //   } else {
+          //     // Send to single or multiple family members
+          //     multipleOrSingleFamilyMembersForReminderUserId(familyId, listItem.reminderUserId, function (err, familyMember) {
+          //       if (err) {
+          //         logging.logError('0', familyId, listItem, err)
+          //         return
+          //       }
+          //       count++
+          //
+          //       // textForReminder(listItem, function (err, text) {
+          //       //   if (err) {
+          //       //   }
+          //       //   text = '⏰ ' + text
+          //       //   console.log('Count++ ' + count + ' pn:' + familyMember.phoneNumber + ' ' + familyMember.name + ':' + text)
+          //       //   logging.log(familyMember.phoneNumber, familyId, text, 'Reminder', '')
+          //       //   sendSms.sendSms(familyMember.phoneNumber, text, function () {
+          //       //     count--
+          //       //     console.log('Count-- ' + count)
+          //       //     if (count === 0) {
+          //       //       process.exit()
+          //       //     }
+          //       //   })
+          //       // })
+          //     })
+          //   }
+          // })
         }
       })
+      console.log('Promises: ' + listItemPromises.length)
+      Q.all(listItemPromises)
+        .then(function (result) {
+          console.log(result)
+          console.log('*** COMPLETE ***')
+          process.exit()
+        }, function (error) {
+          console.error(error)
+          console.log('*** COMPLETE IN ERROR ***')
+          process.exit()
+        })
     }
   })
 }
 
-function multipleOrSingleFamilyMembersForReminderUserId (familyId, reminderUserId, callback) {
-  console.log('FAM ID:' + familyId + ' UserId:' + reminderUserId)
-  if (reminderUserId !== config.allFamilyMembersID) {
-    mongoOp.FamilyMembers.findOne({'familyId': familyId, 'userId': reminderUserId}, function (err, familyMember) {
-      console.log('FAM-SINGLE pn:' + familyMember.phoneNumber + ' ' + familyMember.name)
-      callback(err, familyMember)
-    })
-  } else {
-    mongoOp.FamilyMembers.find({'familyId': familyId}, function (err, familyMembers) {
-      if (err) {
-        callback(err)
-      } else {
-        familyMembers.forEach(function (familyMember) {
-          console.log('FAM-MUTLI pn:' + familyMember.phoneNumber + ' ' + familyMember.name)
-          callback(null, familyMember)
-        })
-      }
-    })
-  }
-}
+// function multipleOrSingleFamilyMembersForReminderUserId (familyId, reminderUserId, callback) {
+//   console.log('FAM ID:' + familyId + ' UserId:' + reminderUserId)
+//   if (reminderUserId !== config.allFamilyMembersID) {
+//     mongoOp.FamilyMembers.findOne({'familyId': familyId, 'userId': reminderUserId}, function (err, familyMember) {
+//       console.log('FAM-SINGLE pn:' + familyMember.phoneNumber + ' ' + familyMember.name)
+//       callback(err, familyMember)
+//     })
+//   } else {
+//     mongoOp.FamilyMembers.find({'familyId': familyId}, function (err, familyMembers) {
+//       if (err) {
+//         callback(err)
+//       } else {
+//         familyMembers.forEach(function (familyMember) {
+//           console.log('FAM-MUTLI pn:' + familyMember.phoneNumber + ' ' + familyMember.name)
+//           callback(null, familyMember)
+//         })
+//       }
+//     })
+//   }
+// }
 
-function multipleOrSingleFamilyMembersForReminderUserIdPromise (familyId, reminderUserId, callback) {
+// function multipleOrSingleFamilyMembersForReminderUserIdPromise2 (listItem) {
+//   var promise
+//   console.log('FAM ID:' + listItem.familyId + ' UserId:' + listItem.reminderUserId)
+//   if (listItem.reminderUserId !== config.allFamilyMembersID) {
+//     promise = mongoOp.FamilyMembers.findOne({'familyId': listItem.familyId, 'userId': listItem.reminderUserId})
+//       .exec()
+//   } else {
+//     promise = mongoOp.FamilyMembers.find({'familyId': listItem.familyId})
+//       .exec()
+//   }
+//   return promise
+// }
+
+function multipleOrSingleFamilyMembersForReminderUserIdPromise (listItem) {
   var familyMembersDeferred = Q.defer()
-  console.log('FAM ID:' + familyId + ' UserId:' + reminderUserId)
-  if (reminderUserId !== config.allFamilyMembersID) {
-    mongoOp.FamilyMembers.findOne({'familyId': familyId, 'userId': reminderUserId}, function (err, familyMember) {
+  console.log('FAM ID:' + listItem.familyId + ' UserId:' + listItem.reminderUserId)
+  if (listItem.reminderUserId !== config.allFamilyMembersID) {
+    mongoOp.FamilyMembers.findOne({'familyId': listItem.familyId, 'userId': listItem.reminderUserId}, function (err, familyMember) {
       console.log('FAM-SINGLE pn:' + familyMember.phoneNumber + ' ' + familyMember.name)
       if (err) {
         return familyMembersDeferred.reject(err)
       } else {
-        return familyMembersDeferred.resolve(familyMember)
+        return familyMembersDeferred.resolve([familyMember, listItem])
       }
     })
   } else {
-    mongoOp.FamilyMembers.find({'familyId': familyId}, function (err, familyMembers) {
+    mongoOp.FamilyMembers.find({'familyId': listItem.familyId}, function (err, familyMembers) {
       if (err) {
-        callback(err)
+        return familyMembersDeferred.reject(err)
       } else {
         familyMembers.forEach(function (familyMember) {
           console.log('FAM-MUTLI pn:' + familyMember.phoneNumber + ' ' + familyMember.name)
-          callback(null, familyMember)
         })
+        var result = familyMembers.map(function (familyMember) {
+          console.log('QQ: ' + familyMember + ' WW: ' + listItem)
+          return ({familyMember, listItem})
+        })
+        return familyMembersDeferred.resolve(result)
       }
     })
   }
+  return familyMembersDeferred.promise
 }
 
 function textForReminder (listItem, callback) {
@@ -167,4 +207,40 @@ function smsPromise (messageDetails) {
   })
 }
 
+// TODO: Move
 processScheduler()
+
+// Attempt 2 : Use mongoose promises
+function deleteListItemByNamePromise (listItem) {
+  var promise = mongoOp.ListItems.remove(listItem)
+    .exec()
+    .then(function (removeResult) {
+      console.log('XB2: ' + removeResult)
+      if (removeResult.result.n !== 1) {
+        throw new Error(listItem.listItemName + " doesn't exist in #" + listItem.listKey + ' 😦.')
+      } else {
+        console.log('---- Removed ' + listItem.listItemName + ' ' + removeResult.result.n)
+        return listItem
+      }
+    })
+  return promise
+}
+
+// MOVE TO SCHEDULEITEM
+// function deleteListItemByNamePromise (listItem) {
+//   var deferred = Q.defer()
+//   console.log('B2: ' + listItem)
+//   mongoOp.ListItems.remove({'listKey': listItem.listKey, 'listItemName': listItem.listItemName, 'familyId': listItem.familyId}, function (err, removeResult) {
+//     console.log('C3 err:' + err)
+//     if (err) {
+//       return deferred.reject('Error removing ' + listItem.listItemName + ' 😦')
+//     }
+//     console.log('----removed ' + listItem.listItemName + ' ' + removeResult.result.n)
+//     if (removeResult.result.n === 0) {
+//       return deferred.reject(listItem.listItemName + " doesn't exist in #" + listItem.listKey + ' 😦.')
+//     } else {
+//       return deferred.resolve(listItem)
+//     }
+//   })
+//   return deferred.promise
+// }
