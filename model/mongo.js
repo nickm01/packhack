a9dfbfe9c20b98d8efe11e6ac49f91095d45d9fb
Nick McConnell <nicholasgmcconnell@gmail.com>
2017-04-20 21:15:04 -0500

    change to mongo.js to allow for mocha watch to work by moving the schema creation into the initialize

--- model/mongo.js
+++ model/mongo.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose')
 mongoose.Promise = require('q').Promise
 
+var Lists, ListItems, FamilyMembers, Logs
+
 const intialize = () => {
   // TODO: Shouldn't be saved here
   mongoose.connect('mongodb://heroku_x61dp8bp:12f6g0oe2r8hn13jnad3m5or49@ds161495.mlab.com:61495/heroku_x61dp8bp')
@@ -8,50 +10,50 @@ const intialize = () => {
   db.on('error', console.error.bind(console, 'connection error:'))
   db.once('open', function () {
     console.log('db connected')
+
+    // Lists
+    const listsSchema = mongoose.Schema({
+      'listKey': String,
+      'listDescription': String,
+      'familyId': Number
+    }, { versionKey: false })
+    Lists = mongoose.model('Lists', listsSchema, 'Lists')
+
+    // ListItems
+    const listItemsSchema = mongoose.Schema({
+      'listKey': String,
+      'listItemName': String,
+      'familyId': Number,
+      'reminderWhen': String,
+      'reminderUserId': Number,
+      'reminderTitle': String,
+      'reminderListKey': String
+    }, { versionKey: false })
+    ListItems = mongoose.model('ListItems', listItemsSchema, 'ListItems')
+
+    // FamilyMembers
+    const familyMemberSchema = mongoose.Schema({
+      'userId': Number,
+      'familyId': Number,
+      'name': String,
+      'phoneNumber': String,
+      'description': String,
+      'timeZone': String
+    }, { versionKey: false })
+    FamilyMembers = mongoose.model('FamilyMembers', familyMemberSchema, 'FamilyMembers')
+
+    // Logs
+    const logsSchema = mongoose.Schema({
+      'phoneNumber': String,
+      'familyId': Number,
+      'message': String,
+      'dateTime': String,
+      'type': String,
+      'response': String
+    }, { versionKey: false })
+    Logs = mongoose.model('Logs', logsSchema, 'Logs')
   })
 }
-
-// Lists
-const listsSchema = mongoose.Schema({
-    'listKey' : String,
-    'listDescription' : String,
-    'familyId' : Number
-}, { versionKey: false })
-const Lists = mongoose.model('Lists', listsSchema, 'Lists')
-
-// ListItems
-const listItemsSchema = mongoose.Schema({
-    'listKey' : String,
-    'listItemName' : String,
-    'familyId' : Number,
-    'reminderWhen': String,
-    'reminderUserId': Number,
-    'reminderTitle': String,
-    'reminderListKey': String
-}, { versionKey: false })
-const ListItems = mongoose.model('ListItems', listItemsSchema, 'ListItems')
-
-// FamilyMembers
-const familyMemberSchema = mongoose.Schema({
-    'userId': Number,
-    'familyId': Number,
-    'name': String,
-    'phoneNumber': String,
-    'description': String,
-    'timeZone': String
-}, { versionKey: false })
-const FamilyMembers = mongoose.model('FamilyMembers', familyMemberSchema, 'FamilyMembers')
-
-// Logs
-const logsSchema = mongoose.Schema({
-    'phoneNumber' : String,
-    'familyId' : Number,
-    'message' : String,
-    'dateTime' : String,
-    'type' : String,
-    'response': String
-}, { versionKey: false })
-const Logs = mongoose.model('Logs', logsSchema, 'Logs')
 
 module.exports = {
   intialize,
