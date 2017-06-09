72f3caa9d7a523b1be3569df40ce80f6d8880c30
Nick McConnell <nicholasgmcconnell@gmail.com>
2017-06-09 15:35:54 -0400

    add test logging

--- model/lists.promises.js
+++ model/lists.promises.js
@@ -1,6 +1,10 @@
 const mongoOp = require('./mongo')
 
 const findOnePromise = ({listKey, familyId}) => {
+  console.log('11')
+  console.log(listKey)
+  console.log(familyId)
+  console.log('11b')
   return mongoOp.Lists.findOne({listKey, familyId})
     .exec()
 }
