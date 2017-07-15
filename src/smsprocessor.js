ccdc2cd1812f54a2bd71288502a9d1e623174823
Nick McConnell <nicholasgmcconnell@gmail.com>
2017-07-15 01:01:13 +0100

    improve logging

--- src/smsprocessor.js
+++ src/smsprocessor.js
@@ -4,13 +4,17 @@ var Q = require('q')
 
 module.exports.sendSmsPromise = function (data, to, message) {
   const deferred = Q.defer()
+  console.log('smsprocessor1')
   client.messages.create({body: message, to: to, from: config.sendingNumber}, (err, data) => {
     if (err) {
+      console.log('smsprocessor2')
       data.systemError = err
       deferred.reject(data)
     } else {
+      console.log('smsprocessor3')
       deferred.resolve(data)
     }
   })
+  if (deferred.then) { console.log('smsprocessor-then-exists') }
   return deferred
 }
