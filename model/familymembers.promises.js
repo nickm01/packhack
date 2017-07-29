3bc6179046208257255035789109cdeaae3c7a99
Nick McConnell <nicholasgmcconnell@gmail.com>
2017-07-28 20:40:32 -0400

    fix all family member processing

--- model/familymembers.promises.js
+++ model/familymembers.promises.js
@@ -2,7 +2,7 @@ const mongoOp = require('./mongo')
 const modelConstants = require('./modelconstants')
 
 const findFromNameFamilyPromise = (name, familyId) => {
-  const filter = name === modelConstants.allFamilyMembersName ? {name, familyId} : {familyId}
+  const filter = name === modelConstants.allFamilyMembersName ? {familyId} : {name, familyId}
   return mongoOp.FamilyMembers.find(filter)
     .exec()
 }
