a615d1b2435ecf87e86a0c2ffaf87c6a79b05466
Nick McConnell <nicholasgmcconnell@gmail.com>
2017-08-19 22:05:15 -0400

    better logging

--- src/smsprocessor.js
+++ src/smsprocessor.js
@@ -5,10 +5,11 @@ const errors = require('./errors')
 module.exports.sendSmsPromise = function (data, to, message) {
   console.log('smsprocessor1')
   console.log(message)
-  return client.messages.create({body: message, to: to, from: config.sendingNumber})
+  const params = {body: message, to: to, from: config.sendingNumber}
+  return client.messages.create(params)
     .then(result => {
       console.log('smsprocessor2')
-      console.log(result)
+      console.log(params)
       return data
     }, err => {
       console.log('smsprocessor3')
