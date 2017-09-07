4f1ecd1f97f6011958d3cc1cffa9ec02cc2b3bf9
Nick McConnell <nicholasgmcconnell@gmail.com>
2017-09-07 16:43:43 -0700

    better caching to support delete ignore, error ignore and reminder ignore

--- src/twilio.route.js
+++ src/twilio.route.js
@@ -1,6 +1,8 @@
 const twilio = require('twilio')
 const textProcessor = require('./textprocessor')
 const logger = require('winston')
+const config = require('../config')
+const commandTypes = require('./commandtypes')
 
 const route = (request, response) => {
   const bodyText = request.query['Body'].toLowerCase()
@@ -21,20 +23,35 @@ const route = (request, response) => {
     now: new Date((new Date()).getTime() - 1000 * 60) // 1 minute in the past
   }
   textProcessor.processTextPromise(data).then(result => {
-    logger.log('info', '*** after processTextpromise', result) // TODO: Better Logging
-    cacheListName(result.list, response) // TODO: Make sure this isn't the case for delete
+    logger.log('info', '*** after processTextpromise', result)
+    cacheListName(result, response) // TODO: Make sure this isn't the case for delete
     sendSMSResponse(result.responseText, response)
   })
 }
 
 const sendSMSResponse = (responseText, response) => {
-  var twilioResponse = new twilio.TwimlResponse()
+  const twilioResponse = new twilio.TwimlResponse()
   twilioResponse.message(responseText)
   response.send(twilioResponse.toString())
 }
 
-const cacheListName = (listName, response) => {
-  response.cookie('listName', listName, {maxAge: 1000 * 60 * 60 * 12})
+const cacheListName = (data, response) => {
+  // If there's been an error - only do NOT cache if the list name is now different
+  // or if there is a delete list command
+  if (data.errorMessage && data.listName !== data.cacheListName ||
+      data.commandTypes === commandTypes.deleteList
+  ) {
+    return
+  }
+
+  // If reminder, cache the old list key
+  let listName
+  if (data.listName === config.remindersListKey) {
+    listName = data.cacheListName
+  } else {
+    listName = data.listName
+  }
+  response.cookie('listName', listName, {maxAge: 1000 * 60 * 60 * 72})
 }
 
 module.exports = {
