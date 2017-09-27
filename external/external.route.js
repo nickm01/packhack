76627c60e8a7082c5ed29f5d31a9565001935f46
Nick McConnell <nicholasgmcconnell@gmail.com>
2017-09-26 22:01:43 -0700

    add call to db for basic list items

--- external/external.route.js
+++ external/external.route.js
@@ -1,11 +1,19 @@
 const logger = require('winston')
+const listItems = require('../model/listitems')
 
 const route = (request, response) => {
   logger.log('info', request)
-  response.json({
-    hello: '123',
-    goodbye: 'abc'
-  })
+  const data = {
+    list: 'shopping',
+    familyId: 1
+  }
+  listItems.findPromise(data)
+    .then(result => {
+      const listItemNames = result.listItems.map(listItem => {
+        return listItem.listItemName
+      })
+      response.json(listItemNames)
+    })
 }
 
 module.exports = {
