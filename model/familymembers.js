f5689c9014ffbf501ece3bafb69f6f2c8d69643d
Nick McConnell <nicholasgmcconnell@gmail.com>
2020-01-13 22:19:56 -0800

    correction

--- model/familymembers.js
+++ model/familymembers.js
@@ -51,11 +51,11 @@ const saveNewFamilyMemberPromise = (data) => {
   logger.log('debug', '___familymembers_saveNewFamilyMemberPromise', data)
   const familyMember = {
     // userId
-    familyId = data.familyId
-    name = data.name
-    description = data.description
-    phoneNumber = data.phonerNumber
-    timeZone = data.timeZone || 'America/New_York'
+    familyId = data.familyId,
+    name = data.name,
+    description = data.description,
+    phoneNumber = data.phonerNumber,
+    timeZone = data.timeZone || 'America/New_York',
     verificationNumber = data.verificationNumber
   }
   return familyMembersPromises.saveNewFamilyMemberPromise(familyMember)
